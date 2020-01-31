e261a578088f90612fc36d483c189ffc6aeb074c
Yongwoon Kim <righteous0319@daum.net>
2020-02-01 00:07:55 +0900

    CrossOrigin

--- src/main/java/com/example/demo5/DemoApplication.java
+++ src/main/java/com/example/demo5/DemoApplication.java
@@ -2,13 +2,16 @@ package com.example.demo5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+// @CrossOrigin(origins = "http://localhost:18080")
 public class DemoApplication {
 
+	// @CrossOrigin(origins = "http://localhost:18080")
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";

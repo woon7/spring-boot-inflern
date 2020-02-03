54ee329445179c0c346cbf8b8f20fb66f17f1707
woon7 <>
2020-02-03 15:17:02 +0900

    H2

---
+++ src/main/java/com/example/data/H2Runner.java
@@ -0,0 +1,40 @@
+package com.example.data;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * H2Runner
+ */
+@Component
+public class H2Runner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            connection.getMetaData().getURL();
+            connection.getMetaData().getUserName();
+            
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+            statement.executeUpdate(sql);
+        }
+
+        jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'keesun')");
+    }
+
+}

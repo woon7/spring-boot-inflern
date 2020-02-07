18e1381fdca2bb56dd20fbbdcde202752224fbd6
woon7 <>
2020-02-07 12:56:40 +0900

    Database Migration

--- src/main/java/com/example/data2/account/Account.java
+++ src/main/java/com/example/data2/account/Account.java
@@ -20,6 +20,8 @@ public class Account {
 
     private String email;
 
+    private boolean active;
+
     public Long getId() {
         return id;
     }
@@ -50,6 +52,14 @@ public class Account {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     @Override

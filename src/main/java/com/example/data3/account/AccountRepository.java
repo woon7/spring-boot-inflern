0a1b4558f0026c88d8482d8951e07c669260d8dc
woon7 <>
2020-02-07 13:45:41 +0900

    Redis

---
+++ src/main/java/com/example/data3/account/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.example.data3.account;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * AccountRepository
+ */
+public interface AccountRepository extends CrudRepository<Account, String> {
+
+    
+}

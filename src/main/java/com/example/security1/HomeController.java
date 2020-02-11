ee32b2712a1418283bd954035febece1b241869d
woon7 <>
2020-02-11 17:42:22 +0900

    Security

---
+++ src/main/java/com/example/security1/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.security1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * HomeController
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+    
+    @GetMapping("/my")
+    public String my() {
+        return "my";
+    }
+}
---
+++ src/main/java/com/example/security1/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.security1;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * WebSecurityConfig
+ * 만들지 않아도 기본 설정이 돼있음
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}

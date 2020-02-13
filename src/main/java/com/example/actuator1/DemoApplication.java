b3a7b7467ec75e84b819282ba83ba3ea293b7543
woon7 <>
2020-02-13 17:00:39 +0900

    Actuator

--- src/main/java/com/example/actuator1/DemoApplication.java
+++ src/main/java/com/example/actuator1/DemoApplication.java
@@ -2,9 +2,15 @@ package com.example.actuator1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	};
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);

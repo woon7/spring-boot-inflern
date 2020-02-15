ce9728fb195bddfa4893a5a1ee841f8e42b9855e
Yongwoon Kim <righteous0319@daum.net>
2020-02-15 20:36:07 +0900

    Admin Server

--- src/main/java/com/example/actuator1_2/DemoApplication.java
+++ src/main/java/com/example/actuator1_2/DemoApplication.java
@@ -3,7 +3,10 @@ package com.example.actuator1_2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAdminServer
 public class DemoApplication {
 
 	public static void main(String[] args) {
